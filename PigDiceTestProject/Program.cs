973c3e930b7bbf8f4764cd624cfb69a954f25e6f
Titus Moore <titusdmoore@gmail.com>
2019-07-26 15:04:17 -0400

    Fixed bugs and added support for A and G

--- PigDiceTestProject/Program.cs
+++ PigDiceTestProject/Program.cs
@@ -7,30 +7,79 @@ namespace PigDiceTestProject {
             int rollInt = 0;
             bool play = true;
             int total = 0;
+            int highScore = 0;
+            int autoIdx = 0;
+            bool auto = false;
 
             do {
                 string playString = null;
-                Console.WriteLine($"Do you wish to play the game?{Environment.NewLine}[Y] for Yes{Environment.NewLine}[N] for No");
-                playString = Convert.ToString(Console.ReadKey());
-                playString = playString.ToUpper();
+                char playChar = 'c';
+                Console.WriteLine($"Do you wish to play the game?{Environment.NewLine}[Y] for Yes{Environment.NewLine}[N] for No" +
+                    $"{Environment.NewLine}[A] for Running Auto for 100 times{Environment.NewLine}[G] if you have time and want to go for Gregs Record");
+
+                if (total != 0) {
+                    Console.WriteLine($"{Environment.NewLine}Your Last Score was {total}{Environment.NewLine}" +
+                        $"The Highest Score this run was {highScore}");
+                }
+
+                playChar = Console.ReadKey(true).KeyChar;
+                playString = Convert.ToString(playChar).ToUpper();
+                Console.WriteLine(playString);
                 Console.Clear();
 
-                // For future debugging, right now we are not geting the string 'Y' 
-                Console.WriteLine(playString);
-                if (playString == "Y") {
-                    play = true;
-                }else {
-                    play = false;
+                switch (playString) {
+                    case "Y":
+                        play = true;
+                        total = 0;
+                        Console.Clear();
+                        break;
+                    case "A":
+                        auto = true;
+                        autoIdx = 0;
+                        total = 0;
+                        break;
+                    case "G":
+                        auto = true;
+                        autoIdx = 0;
+                        total = 0;
+                        break;
+                    default:
+                        play = false;
+                        Console.Clear();
+                        break;
                 }
 
                 do {
                     rollInt = roll.Next(1, 6);
-
                     total += rollInt;
-                } while (rollInt != 1);
-            } while (play);
+                    if (highScore < total) {
+                        highScore = total;
+                    }
+                } while (rollInt != 1 && !auto);
 
-            Console.WriteLine($"Your score is {total}");
+
+                //Runs for case "A"
+                while (auto && autoIdx < 101) {
+                    rollInt = roll.Next(1, 6);
+                    total += rollInt;
+                    if (highScore < total) {
+                        highScore = total;
+                    }
+                    autoIdx++;
+                }
+
+                //Runs for Case "G"
+                while (auto && autoIdx < 10000001) {
+                    rollInt = roll.Next(1, 6);
+                    total += rollInt;
+                    if (highScore < total) {
+                        highScore = total;
+                    }
+                    autoIdx++;
+                }
+
+            } while (play);
+            Console.WriteLine("Thanks for playing!");
         }
     }
 }
